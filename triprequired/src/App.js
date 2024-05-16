555023a6d2547b90254bbe2ffa8ad12b4b052660
rahul14455 <42345755+rahul14455@users.noreply.github.com>
2024-05-16 12:03:58 +0530

    fcommit

---
+++ triprequired/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
